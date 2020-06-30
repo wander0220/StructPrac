ceee11512a3509fc784f00699d2ccd99cd8b2ef2
wander0220 <s2018s26@e-mirim.hs.kr>
2020-06-30 09:47:35 +0900

    초기 커밋.

---
+++ StructPrac/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructPrac
+{
+    struct Point
+    {
+        public int x;
+        public int y;
+        public Program program;
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            this.program = null;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point; //== int x
+        }
+    }
+}
