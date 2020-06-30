4a99641175e69dfcfa9366d1b42b795f5e677db3
wander0220 <s2018s26@e-mirim.hs.kr>
2020-06-30 10:15:32 +0900

    class와 struct의 차이 100,200 100,200 10,20 100,200

--- StructPrac/Program.cs
+++ StructPrac/Program.cs
@@ -18,6 +18,30 @@ namespace StructPrac
             this.program = null;
         }
     }
+
+    class Pointclass
+    {
+        public int x;
+        public int y;
+        public Pointclass(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    struct PointStruct
+    {
+        public int x;
+        public int y;
+        public PointStruct(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
@@ -26,6 +50,27 @@ namespace StructPrac
             //point = new Point(); // x=0,y=0; new 키워드 쓰면 기본값으로 초기화해준다.
             point.x = 10;
             point.y = 10;
+
+
+            Pointclass pointClassA = new Pointclass(10, 20);
+            Pointclass pointClassB = pointClassA;
+
+            pointClassB.x = 100;
+            pointClassB.y = 200;
+
+            Console.WriteLine("pointClassA : " + pointClassA.x + "," + pointClassA.y);
+            Console.WriteLine("pointClassB : " + pointClassB.x + "," + pointClassB.y);
+
+
+            PointStruct pointStructA = new PointStruct(10, 20);
+            PointStruct pointStructB = pointStructA;
+
+            pointStructB.x = 100;
+            pointStructB.y = 200;
+
+            Console.WriteLine("pointStructA : " + pointStructA.x + "," + pointStructA.y);
+            Console.WriteLine("pointStructB : " + pointStructB.x + "," + pointStructB.y);
+
         }
     }
 }
